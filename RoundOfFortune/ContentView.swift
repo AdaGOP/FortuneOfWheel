af861febb30137002e944dcf56c4bf894152f7b6
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-04-18 21:47:43 +0700

    Initial Commit

---
+++ RoundOfFortune/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoundOfFortune
+//
+//  Created by khoirunnisa' rizky noor fatimah on 18/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoundOfFortune/RoundOfFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundOfFortuneApp.swift
+//  RoundOfFortune
+//
+//  Created by khoirunnisa' rizky noor fatimah on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RoundOfFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
