82b3b419cf5e30ad21b1523cca181ba81522c9b3
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-06-07 14:03:59 +0700

    finish layout for iOS

--- RoundOfFortune/ContentView.swift
+++ RoundOfFortune/ContentView.swift
@@ -16,23 +16,22 @@ struct ContentView: View {
     @State private var textOption: String = ""
     
     // Array to store the options for the wheel
-    @State private var optionsListWithIndex: [theValue : String] = [:]
+    @State private var optionsListWithIndex: [Int : String] = [:]
     @State private var valueIndex: Int = 0
-    @State var array: [theValue] = [theValue(val: "1"),
-                             theValue(val: "2"),
-                             theValue(val: "3")]
+    @State var arrayIndeks: [theValue] = []
     
     
     var body: some View {
-        ZStack(alignment: .center) {
+        ZStack() {
             Color.orange.opacity(0.4).ignoresSafeArea()
                 .hueRotation(Angle(degrees: degree))
-            VStack{
+            VStack(alignment: .center, spacing: 10){
+                Spacer()
                 ZStack(){
-                    WheelView(degree: $degree, array: $array, circleSize: 300)
+                    WheelView(degree: $degree, array: $arrayIndeks, circleSize: 300)
                         .offset(y: -100)
                         .shadow(color: .white, radius: 4, x: 0, y: 0)
-                    if optionsListWithIndex.count == 0 {
+                    if arrayIndeks.count == 0 {
                         VStack {
                             Text("No data is available.")
                             Text("Add some below")
@@ -41,6 +40,26 @@ struct ContentView: View {
                         .foregroundColor(Color.primary)
                         .offset(y: -100)
                     }
+                }
+                
+                HStack(){
+                    Spacer()
+                    Text("Option List")
+                        .font(.title2)
+                        .bold()
+                        .frame(alignment: .leading)
+                    Spacer()
+                        .frame(width: 165)
+                    Button {
+                        optionsListWithIndex.removeAll()
+                        arrayIndeks.removeAll()
+                        valueIndex = 0
+                    } label: {
+                        Label("Reset", systemImage: "repeat")
+                    }
+                    .foregroundColor(.red)
+                    .cornerRadius(8)
+                    Spacer()
                     
                 }
                 
@@ -52,9 +71,12 @@ struct ContentView: View {
                     )
                     .font(.system(size: 15))
                     .textFieldStyle(.roundedBorder)
+                    
                     Button(action: {
                         valueIndex += 1
-                        optionsListWithIndex[theValue(val: "\(valueIndex)")] = textOption
+                        optionsListWithIndex[valueIndex] = textOption
+                        arrayIndeks.append(theValue(val: "\(valueIndex)"))
+                        textOption = ""
                     }) {
                         Label("Add option", systemImage: "plus")
                             .font(.system(size: 15))
@@ -63,24 +85,23 @@ struct ContentView: View {
                     .background(.blue)
                     .foregroundColor(.white)
                     .cornerRadius(8)
-                    Spacer(minLength: 20.0)
+                    Spacer()
                 }
-                
-                
-                ForEach(Array(optionsListWithIndex.values).sorted(), id: \.self){ textValue in
-                    VStack(){
-                        HStack(){
-                            Text(findKeyForValue(value: textValue, dictionary: optionsListWithIndex) ?? "")
-                            Text(textValue)
-                                .font(.system(.body))
+                ScrollView{
+                    ForEach(arrayIndeks, id: \.self) { indeks in
+                        VStack{
+                            HStack{
+                                Text("\(indeks.val)")
+                                Text(optionsListWithIndex[Int(indeks.val) ?? 0] ?? "")
+                                    .font(.system(.body))
+                            }
+                            .frame(width: 340, height: 20, alignment: .leading)
+                            Divider()
                             
                         }
-                        Divider()
                     }
-                    
                 }
-                
-                
+                .frame(width: 350, height: 150, alignment: .center)
             }
             
         }
@@ -88,7 +109,6 @@ struct ContentView: View {
     
     func findKeyForValue(value: String, dictionary: [theValue: String]) ->String?
     {
-        print(Array(optionsListWithIndex.values))
         for (key, string) in dictionary
         {
             if (string.contains(value))
--- RoundOfFortune/WheelView.swift
+++ RoundOfFortune/WheelView.swift
@@ -8,42 +8,49 @@
 import SwiftUI
 
 struct WheelView: View {
-    // Circle Radius
-    @State var radius: Double = 150
-    
-    // Direction of swipe
-    @State var direction = Direction.left
-    
-    // index of the number at the bottom of the circle
-    @State var chosenIndex = 0
-    
-    // degree of circle and hue
-    @Binding var degree: Double
-    
-    @Binding var array: [theValue]
+    @State var radius: Double = 150 // Circle Radius
+    @State var direction = Direction.left // Direction of swipe
+    @State var chosenIndex = 0 // Index of the chosen element
+    @Binding var degree: Double // Degree of circle
+    @Binding var array: [theValue] // Element showed in the circle
     let circleSize: Double
     
     var body: some View {
         ZStack {
+            // Store the angle per part, if we have n partition, its angle will be (2*Pi)/n - Pi = 180 degree
             let anglePerCount = Double.pi * 2.0 / Double(array.count)
+            
+            // Here we define the gesture to move the wheels
             let drag = DragGesture()
                 .onEnded { value in
+                    // Check possibility range from the starting potition to define move direction
                     if value.startLocation.x > value.location.x + 10 {
                         direction = .left
                     } else if value.startLocation.x < value.location.x - 10 {
                         direction = .right
                     }
-                    moveWheel()
+                    
+                    // Ensure if there is no data, no move happens
+                    if array.count != 0 {
+                        moveWheel()
+                    }
                 }
             
             // MARK: WHEEL STACK - BEGINNING
             ZStack{
+                // Create Circle view with gradient and hue rotation effect color
                 Circle().fill(EllipticalGradient(colors: [.orange, .yellow]))
                     .hueRotation(Angle(degrees: degree))
-                ForEach(0 ..< array.count) { index in
+                
+                // Create view for each partition
+                ForEach(0 ..< array.count, id: \.self) { index in
+                    // Define angle per partition from initial Circle degree at 0
                     let angle = Double(index) * anglePerCount
+                    // Define (x,y) position depends on the angle (pythagoras) to set up the distance (offset)
                     let xOffset = CGFloat(radius * cos(angle))
                     let yOffset = CGFloat(radius * sin(angle))
+                    
+                    // Create the view to display the option index
                     Text("\(array[index].val)")
                         .rotationEffect(Angle(degrees: degree))
                         .offset(x: xOffset, y: yOffset)
@@ -66,6 +73,7 @@ struct WheelView: View {
     
     func moveWheel() {
         withAnimation(.spring()){
+            let randomInt = Int.random(in: 0 ..< array.count)
             if direction == .left {
                 degree += Double(360/array.count)
                 if chosenIndex == 0 {
@@ -75,7 +83,7 @@ struct WheelView: View {
                 }
             } else {
                 degree -= Double(360/array.count)
-                if chosenIndex == array.count-1 {
+                if chosenIndex == array.count - 1 {
                     chosenIndex = 0
                 } else {
                     chosenIndex += 1
